668c46c894a1c4e6b0c3a363345deea242812ed4
Meecore4 <[alicia.critelli4@gmail.com](mailto:alicia.critelli4@gmail.com)>
2022-12-11 22:31:36 +0100

    version finale :)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// MENU BURGER //
+
 const hamburgerButton = document.querySelector(".nav-toggler");
 const navigation = document.querySelector("nav");
 
@@ -7,3 +9,29 @@ function toggleNav(){
     hamburgerButton.classList.toggle("active")
     navigation.classList.toggle("active")
 }
+
+
+// ANIMATION //
+
+const title = document.querySelector('h4');
+const txt = "Nos chers délégués ♡"
+
+function typewriter(delegue, index){
+
+    if(index < delegue.length){
+
+        setTimeout(() => {
+            title.innerHTML += `${delegue[index]}`
+            typewriter(txt, index + 1)
+        },
+         
+        200);
+     }
+}
+
+setTimeout(() => {
+
+    typewriter(txt, 0)
+},
+
+200);
